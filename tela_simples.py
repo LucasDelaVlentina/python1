bafae1297e47831582914068cc409918ac4ae7eb
LucasDelaVlentina <lucas.valentina@alunos.bpkedu.com.br>
2024-08-07 12:34:13 -0300

    Update tela_simples.py

--- tela_simples.py
+++ tela_simples.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 import webbrowser
 
-# Função para abrir o link no navegador
-def abrir_link():
-    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+# Função para abrir o primeiro link no navegador
+def abrir_primeiro_link():
+    url = "https://www.yout-ube.com/watch?v=dQw4w9WgXcQ"
+    webbrowser.open(url)
+
+# Função para abrir o segundo link no navegador
+def abrir_segundo_link():
+    url = "https://www.yout-ube.com/watch?si=YmiH2ANnW1yTrG6A&v=TZEijhxnXm4&feature=youtu.be"
     webbrowser.open(url)
 
 # Criação da janela principal
@@ -14,9 +19,13 @@ root.title("Tela Simples")
 label = tk.Label(root, text="Olá, Mundo!")
 label.pack(pady=20)
 
-# Criação de um botão
-button = tk.Button(root, text="Clique Aqui", command=abrir_link)
-button.pack(pady=10)
+# Criação do primeiro botão
+button1 = tk.Button(root, text="Clique Aqui", command=abrir_primeiro_link)
+button1.pack(pady=10)
+
+# Criação do segundo botão
+button2 = tk.Button(root, text="Outro Link", command=abrir_segundo_link)
+button2.pack(pady=10)
 
 # Execução do loop principal da interface gráfica
 root.mainloop()
